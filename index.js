d6b170320bb7980177c3ca752bfd0d9010e4aa20
luizgodoy <garciadegodoy@msn.com>
2020-11-13 23:31:21 -0300

    feat: renderizando vídeos no after effects

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
     state: require('./robots/state.js'),
-    image: require('./robots/image.js')
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js')
+
 }
 
 async function inicio() {
@@ -10,7 +12,8 @@ async function inicio() {
     robots.input()
     await robots.text()
     await robots.image()
-
+    await robots.video()
+    
     //const content = robots.state.load()
     //console.dir(content, { depth: null } )
 
